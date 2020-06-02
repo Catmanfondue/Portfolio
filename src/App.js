3fc2f6a1f5152df3db8dea06b7e13d8326384dd1
Zachary Eckert <zaceckert74@gmail.com>
2020-06-02 14:03:37 -0500

    changing color theme again

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ function App() {
         palette: {
           type: prefersDarkMode ? 'dark' : 'light',
           primary: {
-            main: '#4dd69b',
+            main: '#177e89',
           },
           secondary: {
-            main: '#b92446',
+            main: '#222e50',
           },
         },
       });
@@ -33,10 +33,10 @@ function App() {
         palette: {
           type: prefersDarkMode ? 'dark' : 'light',
           primary: {
-            main: '#4dd69b',
+            main: '#177e89',
           },
           secondary: {
-            main: '#b92446',
+            main: '#222e50',
           },
         },
       });
